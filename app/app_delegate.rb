fc06ee574f5a269969e8f8f205e2fed9f19d3c77
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-08-17 19:34:18 +0900

    Add UITabBar

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -4,7 +4,15 @@ class AppDelegate
     @window.makeKeyAndVisible
 
     controller = TapController.alloc.initWithNibName(nil, bundle: nil)
-    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(controller)
+    nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
+
+    other_controller = UIViewController.alloc.initWithNibName(nil, bundle: nil)
+    other_controller.title = "Other"
+    other_controller.view.backgroundColor = UIColor.purpleColor
+
+    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
+    tab_controller.viewControllers = [nav_controller, other_controller]
+    @window.rootViewController = tab_controller
 
     true
   end
