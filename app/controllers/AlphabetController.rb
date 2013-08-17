6d9afdab88285940413d8a8f2a810611669b0ceb
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-08-17 21:09:28 +0900

    Add data for display in UITableView

--- app/controllers/AlphabetController.rb
+++ app/controllers/AlphabetController.rb
@@ -6,5 +6,27 @@ class AlphabetController < UIViewController
 
     @table = UITableView.alloc.initWithFrame(self.view.bounds)
     self.view.addSubview @table
+    @table.dataSource = self
+
+    @data = ("A".."Z").to_a
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    # return the UITableViewCell for the row
+    @reuseIdentifier ||= "CELL_IDENTIFIER"
+
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
+      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
+
+    end
+    
+    cell.textLabel.text = @data[indexPath.row]
+
+    cell
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    # return the number of rows
+    @data.count
   end
 end
