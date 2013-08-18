474318c05b85a3b0428b69b924d8bbd2121cb08a
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-08-18 13:57:43 +0900

    Add some tests

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -7,3 +7,12 @@ describe "Application 'HelloMotion'" do
     @app.windows.size.should == 1
   end
 end
+
+describe "button controller" do
+  tests ButtonController
+
+  it "changes instance variable when button is tapped" do
+    tap 'Test me!'
+    controller.instance_variable_get("@was_tapped").should == true
+  end
+end
