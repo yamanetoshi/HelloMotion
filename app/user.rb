8cd67e833bb490e3b01ee35ca71ff142c68b7860
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-08-18 13:47:23 +0900

    learn some Model objects implementation

---
+++ app/user.rb
@@ -0,0 +1,12 @@
+class User
+  PROPERTIES = [:id, :name, :email]
+  PROPERTIES.each { |prop|
+    attr_accessor prop
+  }
+
+  def initialize(attributes = {})
+    attributes.each { |key, value|
+      self.send("#{key}=", value) if PROPERTIES.member? key.to_sym
+    }
+  end
+end
