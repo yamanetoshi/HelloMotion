8b81a0277199bdc1745d4f95b1d9c78431130bd4
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-08-18 13:27:11 +0900

    Add animate_to_next_point method

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,35 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
+    @window.makeKeyAndVisible
+
+    # the points we're going to animate to
+    @points = [[0, 0], [50, 0], [0, 50], [50, 50]]
+    @current_index = 0
+
+    # usual method of adding views to our window
+    @view = UIView.alloc.initWithFrame [@points[@current_index], [100, 100]]
+    @view.backgroundColor = UIColor.blueColor
+    @window.addSubview(@view)
+
+    animate_to_next_point
+
     true
+  end
+
+  def animate_to_next_point
+    @current_index += 1
+
+    # keep current_index in the range [0,3]
+    @current_index = @current_index % @points.count
+
+    UIView.animateWithDuration(2,
+      animations: lambda {
+         @view.frame = [@points[@current_index], [100, 100]]
+      },
+      completion:lambda {|finished|
+        self.animate_to_next_point
+      }
+    )
   end
 end
